1d899472f7541dddae8f10347a4567d04df20937
FatmahAdel <fatmahalmohsen5@gmail.com>
2026-04-21 19:11:06 +0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "root", "recipedb (2)", 8889);
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db   = "recipedb";
+$port = 8889;
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
